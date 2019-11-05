a955b71cf9f7bdc0968befb21eb3097d6b7857a5
Christophe Boulanger <christophe.boulanger@imio.be>
2019-11-05 13:21:29 +0100

    Add fields for Item content type

--- src/plonemeeting/portal/core/content/item.py
+++ src/plonemeeting/portal/core/content/item.py
@@ -2,59 +2,60 @@
 # from plone.app.textfield import RichText
 # from plone.autoform import directives
 from plone.dexterity.content import Container
+from plone.namedfile import field as namedfile
 # from plone.namedfile import field as namedfile
 from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
 # from zope import schema
 from zope.interface import implementer
+from zope import schema
 
-
-# from plonemeeting.portal.core import _
+from plonemeeting.portal.core import _
+from plonemeeting.portal.core import vocabulary
 
 
 class IItem(model.Schema):
     """ Marker interface and Dexterity Python Schema for Item
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('item.xml')
+    point_number = schema.Text(
+        title=_(u'Point number'),
+        required=True
+    )
+
+    uid = schema.Text(
+        title=_(u'PM UID'),
+        required=True
+    )
+
+    text = schema.RichText(
+        title=_(u'Text'),
+        required=False
+    )
 
     # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    point_type = schema.Choice(
+        title=_(u'Point type'),
+        vocabulary=vocabulary.item_point_type,
+        required=True
+    )
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    category = schema.Choice(
+        title=_(u'Category/Matter'),
+        vocabulary=vocabulary.item_category,
+        required=True
+    )
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
+    annexe = namedfile.NamedBlobFile(
+        title=_(u"annexe", default=u"File"),
+        required=True,
+    )
 
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    description = schema.RichText(
+        title=_(u'Text'),
+        required=False
+    )
 
 
 @implementer(IItem)
