c0e81836b35d4a71c090992e40e430584fa9cd4d
Christophe Boulanger <christophe.boulanger@imio.be>
2019-11-05 13:22:06 +0100

    Add vocabulary file.

---
+++ src/plonemeeting/portal/core/vocabulary.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
+
+from plonemeeting.portal.core import _
+
+item_category = SimpleVocabulary(
+    [SimpleTerm(value=u'locale', title=_(u'Locale')),
+     SimpleTerm(value=u'globale', title=_(u'Globale'))]
+    )
+
+item_point_type = SimpleVocabulary(
+    [SimpleTerm(value=u'normal', title=_(u'Normal')),
+     SimpleTerm(value=u'significant', title=_(u'Significant'))]
+    )
