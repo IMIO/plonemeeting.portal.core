c3d2519b776b4d96b25c1ac0595d84bf1405198c
Antoine Duchêne <antoine.duchene@imio.be>
2019-11-05 14:37:23 +0100

    PEP8 + use Plone locales

--- src/plonemeeting/portal/core/content/annex.py
+++ src/plonemeeting/portal/core/content/annex.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
+from Products.CMFPlone import PloneMessageFactory as plone_
 from plone.dexterity.content import Item
 from plone.namedfile import field as namedfile
 from plone.supermodel import model
-from zope.interface import implementer
-from zope import schema
-
-
 from plonemeeting.portal.core import _
+from zope import schema
+from zope.interface import implementer
 
 
 class IAnnex(model.Schema):
     """ Marker interface and Dexterity Python Schema for Annex
     """
+
     title = schema.TextLine(
-        title=_(u"Title"),
+        title=plone_(u"Title"),
         required=True,
         # readonly=True
     )
