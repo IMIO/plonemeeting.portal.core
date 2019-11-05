a5225a4870bb3cf13fa926c2126946892c279d39
Antoine Duchêne <antoine.duchene@imio.be>
2019-11-05 11:52:15 +0100

    Comment readonly parameter for testing purposes and remove plone.basic behavior from Annex content type

--- src/plonemeeting/portal/core/content/annex.py
+++ src/plonemeeting/portal/core/content/annex.py
@@ -3,6 +3,7 @@ from plone.dexterity.content import Item
 from plone.namedfile import field as namedfile
 from plone.supermodel import model
 from zope.interface import implementer
+from zope import schema
 
 
 from plonemeeting.portal.core import _
@@ -11,8 +12,17 @@ from plonemeeting.portal.core import _
 class IAnnex(model.Schema):
     """ Marker interface and Dexterity Python Schema for Annex
     """
+    title = schema.TextLine(
+        title=_(u"label_title", default=u"Title"),
+        required=True,
+        # readonly=True
+    )
 
-    file = namedfile.NamedBlobFile(title=_(u"File"), required=True)
+    file = namedfile.NamedBlobFile(
+        title=_(u"label_file", default=u"File"),
+        required=True,
+        # readonly=True
+    )
 
 
 @implementer(IAnnex)
