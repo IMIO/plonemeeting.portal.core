6c5e90a5fb7c5d94fa184ec95460faed7b9f7cb5
Christophe Boulanger <christophe.boulanger@imio.be>
2019-11-05 14:50:09 +0100

    Remove plone.basic out of Item.xml and add title field in item and a rich description field

--- src/plonemeeting/portal/core/content/item.py
+++ src/plonemeeting/portal/core/content/item.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 from plone.app.textfield import RichText
+
 # from plone.autoform import directives
 from plone.dexterity.content import Container
+from plone.namedfile import field as namedfile
+
 # from plone.namedfile import field as namedfile
 from plone.supermodel import model
+
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
 # from zope import schema
@@ -12,44 +16,35 @@ from zope import schema
 
 from plonemeeting.portal.core import _
 from plonemeeting.portal.core import vocabulary
+from Products.CMFPlone import PloneMessageFactory as plone_
 
 
 class IItem(model.Schema):
     """ Marker interface and Dexterity Python Schema for Item
     """
 
-    point_number = schema.TextLine(
-        title=_(u'Point number'),
-        required=True
-    )
+    title = schema.TextLine(title=plone_(u"Title"), required=True, readonly=True)
 
-    uid = schema.TextLine(
-        title=_(u'PM UID'),
-        required=True
-    )
+    point_number = schema.TextLine(title=_(u"Point number"), required=True)
 
-    text = RichText(
-        title=_(u'Text'),
-        required=False
-    )
+    uid = schema.TextLine(title=_(u"PM UID"), required=True)
+
+    text = RichText(title=_(u"Text"), required=False)
 
     # directives.widget(level=RadioFieldWidget)
     point_type = schema.Choice(
-        title=_(u'Point type'),
-        vocabulary=vocabulary.item_point_type,
-        required=True
+        title=_(u"Point type"), vocabulary=vocabulary.item_point_type, required=True
     )
 
     category = schema.Choice(
-        title=_(u'Category/Matter'),
-        vocabulary=vocabulary.item_category,
-        required=True
+        title=_(u"Category/Matter"), vocabulary=vocabulary.item_category, required=True
     )
 
-    description = RichText(
-        title=_(u'Text'),
-        required=False
+    annexe = namedfile.NamedBlobFile(
+        title=_(u"annexe", default=u"File"), required=True,
     )
+
+    rich_description = RichText(title=_(u"Text"), required=False)
 
 
 @implementer(IItem)
