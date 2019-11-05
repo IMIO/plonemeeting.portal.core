bb767832d83f15b08724c99470431bb203494d2a
Antoine Duchêne <antoine.duchene@imio.be>
2019-11-05 11:22:53 +0100

    Fix missing import + 'black' formating

--- src/plonemeeting/portal/core/content/annex.py
+++ src/plonemeeting/portal/core/content/annex.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
-# from plone.autoform import directives
 from plone.dexterity.content import Item
-# from plone.namedfile import field as namedfile
+from plone.namedfile import field as namedfile
 from plone.supermodel import model
-# from plone.supermodel.directives import fieldset
-# from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
 from zope.interface import implementer
 
 
@@ -16,10 +11,8 @@ from plonemeeting.portal.core import _
 class IAnnex(model.Schema):
     """ Marker interface and Dexterity Python Schema for Annex
     """
-    file = namedfile.NamedBlobFile(
-        title=_(u'File'),
-        required=True,
-    )
+
+    file = namedfile.NamedBlobFile(title=_(u"File"), required=True)
 
 
 @implementer(IAnnex)
