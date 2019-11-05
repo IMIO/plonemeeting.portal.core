23673dc0887e7c284d1fcb0e4b5af199caa7f7b5
Olivier Delaere <olivier.delaere@imio.be>
2019-11-05 11:41:56 +0100

    Added configuration for Meeting content type

--- src/plonemeeting/portal/core/content/meeting.py
+++ src/plonemeeting/portal/core/content/meeting.py
@@ -6,55 +6,25 @@ from plone.dexterity.content import Container
 from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
-
-# from plonemeeting.portal.core import _
+from plonemeeting.portal.core import _
 
 
 class IMeeting(model.Schema):
     """ Marker interface and Dexterity Python Schema for Meeting
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
+    datetime = schema.DateTime(
+        title=_(u'MeetingDatetime'),
+        requiered=True,
+        readonly=True,
+    )
 
-    # model.load('meeting.xml')
-
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
-
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
-
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    attentees = schema.Text(
+        title=_(u'Assembly'),
+        requiered=True,
+    )
 
 
 @implementer(IMeeting)
