cf8d755d5e9be9908dbe6d67b8e33ad3aba83ae5
Antoine Duchêne <antoine.duchene@imio.be>
2019-11-05 11:11:39 +0100

    Cleanup Annex content type + add file field to it

--- src/plonemeeting/portal/core/content/annex.py
+++ src/plonemeeting/portal/core/content/annex.py
@@ -10,51 +10,16 @@ from plone.supermodel import model
 from zope.interface import implementer
 
 
-# from plonemeeting.portal.core import _
+from plonemeeting.portal.core import _
 
 
 class IAnnex(model.Schema):
     """ Marker interface and Dexterity Python Schema for Annex
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
-
-    # model.load('annex.xml')
-
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
-
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
-
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    file = namedfile.NamedBlobFile(
+        title=_(u'File'),
+        required=True,
+    )
 
 
 @implementer(IAnnex)
