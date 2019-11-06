77b28ae0ace0fefc6ef9590265d901bd542c9907
Laurent Lasudry <lasudry@gmail.com>
2019-11-06 11:51:23 +0100

    Add item default view

---
+++ src/plonemeeting/portal/core/browser/item.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from plone import api
+from plone.dexterity.browser.view import DefaultView
+
+
+class ItemView(DefaultView):
+    """
+    """
+
+    def get_files(self):
+        brains = api.content.find(portal_type="File", context=self.context)
+        return brains
