883223d66c047f5a65a9e42a19cff7e757161980
Antoine Duchêne <antoine.duchene@imio.be>
2019-11-06 12:03:20 +0100

    Add logo to Institution

--- src/plonemeeting/portal/core/content/institution.py
+++ src/plonemeeting/portal/core/content/institution.py
@@ -2,16 +2,16 @@
 
 from collective.z3cform.datagridfield import DataGridFieldFactory
 from collective.z3cform.datagridfield import DictRow
+from plone import namedfile
 from plone.app.textfield import RichText
 from plone.dexterity.browser import add
 from plone.dexterity.browser import edit
 from plone.dexterity.content import Container
 from plone.supermodel import model
+from plonemeeting.portal.core import _
 from zope import schema
 from zope.interface import Interface
 from zope.interface import implementer
-
-from plonemeeting.portal.core import _
 
 
 class ICategoryMappingRowSchema(Interface):
@@ -36,6 +36,8 @@ class IInstitution(model.Schema):
 
     password = schema.Password(title=_(u"Password"), required=False)
 
+    logo = namedfile.NamedBlobImage(title=_(u"Logo"))
+
     meeting_config_id = schema.TextLine(title=_(u"Meeting config ID"), required=False)
 
     info_points_formatting_tal = schema.TextLine(
@@ -54,9 +56,7 @@ class IInstitution(model.Schema):
 
     representatives_mappings = schema.List(
         title=_(u"Representatives mappings"),
-        value_type=DictRow(
-            title=u"Representative mapping", schema=IRepresentativeMappingRowSchema
-        ),
+        value_type=DictRow(title=u"Representative mapping", schema=IRepresentativeMappingRowSchema),
         required=False,
     )
 
