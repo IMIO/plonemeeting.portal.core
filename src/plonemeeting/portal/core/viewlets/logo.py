e31d3611dbc252aebe812a615868524c9cb1d771
Laurent Lasudry <lasudry@gmail.com>
2019-11-15 10:40:18 +0100

    Get logo from institution if any

---
+++ src/plonemeeting/portal/core/viewlets/logo.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from plone import api
+from plone.app.layout.viewlets.common import LogoViewlet
+
+from plonemeeting.portal.core.content.institution import IInstitution
+
+
+class PMLogoViewlet(LogoViewlet):
+
+    def update(self):
+        super(PMLogoViewlet, self).update()
+
+        nav_root = api.portal.get_navigation_root(self.context)
+        if IInstitution.providedBy(nav_root):
+            if not nav_root.logo:
+                return
+            self.navigation_root_title = nav_root.title
+            self.logo_title = nav_root.title
+            self.img_src = u"{0}/@@images/logo".format(nav_root.absolute_url())
