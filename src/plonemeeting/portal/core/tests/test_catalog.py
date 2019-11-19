cd445a3b78c543b65b88ca9378672971910b1734
Laurent Lasudry <lasudry@gmail.com>
2019-11-19 10:39:33 +0100

    Add more indexing tests

--- src/plonemeeting/portal/core/tests/test_catalog.py
+++ src/plonemeeting/portal/core/tests/test_catalog.py
@@ -18,6 +18,7 @@ class TestMeetingWorkflow(unittest.TestCase):
     def setUp(self):
         self.portal = self.layer["portal"]
         self.catalog = self.portal.portal_catalog
+        self.workflow = self.portal.portal_workflow
 
         self.portal.acl_users._doAddUser("manager", "secret", ["Manager"], [])
 
@@ -40,3 +41,32 @@ class TestMeetingWorkflow(unittest.TestCase):
         searchable_text = indexes.get("SearchableText")
         self.assertTrue("test_title" in searchable_text)
         self.assertTrue("test_deliberation" in searchable_text)
+
+    def testMeetingValues(self):
+        brain = api.content.find(context=self.meeting, portal_type="Item")[0]
+        indexes = self.catalog.getIndexDataForRID(brain.getRID())
+        self.assertEqual(self.meeting_item.number, indexes.get("item_number"))
+        self.assertEqual(self.meeting.title, indexes.get("linkedMeetingTitle"))
+        self.assertEqual(self.meeting.UID(), indexes.get("linkedMeetingUID"))
+        self.assertEqual(indexes.get("linkedMeetingDate"), 1081567825)
+        self.assertEqual(str(self.meeting.date_time.year), indexes.get("year"))
+
+    def testMeetingReviewState(self):
+        brain = api.content.find(context=self.meeting, portal_type="Item")[0]
+        indexes = self.catalog.getIndexDataForRID(brain.getRID())
+        self.assertEqual(
+            api.content.get_state(self.meeting), indexes.get("linkedMeetingReviewState")
+        )
+        self.assertEqual(indexes.get("linkedMeetingReviewState"), "private")
+        self.workflow.doActionFor(self.meeting, "send_to_project")
+        indexes = self.catalog.getIndexDataForRID(brain.getRID())
+        self.assertEqual(
+            api.content.get_state(self.meeting), indexes.get("linkedMeetingReviewState")
+        )
+        self.assertEqual(indexes.get("linkedMeetingReviewState"), "in_project")
+        self.workflow.doActionFor(self.meeting, "publish")
+        indexes = self.catalog.getIndexDataForRID(brain.getRID())
+        self.assertEqual(
+            api.content.get_state(self.meeting), indexes.get("linkedMeetingReviewState")
+        )
+        self.assertEqual(indexes.get("linkedMeetingReviewState"), "published")
