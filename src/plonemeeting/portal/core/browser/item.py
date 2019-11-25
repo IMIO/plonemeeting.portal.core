9fa5cde41e40e405489d35a51354f1fef297f752
Laurent Lasudry <lasudry@gmail.com>
2019-11-25 15:44:48 +0100

    Black / cleanup

--- src/plonemeeting/portal/core/browser/item.py
+++ src/plonemeeting/portal/core/browser/item.py
@@ -15,14 +15,16 @@ class ItemView(DefaultView):
 
     def get_global_category(self):
         item_local_category = self.context.category
-        utils_view = getMultiAdapter((self.context, self.request), name='utils_view')
+        utils_view = getMultiAdapter((self.context, self.request), name="utils_view")
         institution = self.context.aq_parent.aq_parent
-        global_category = ''
+        global_category = ""
         for mapping in institution.categories_mappings:
-            if mapping['local_category_id'] == item_local_category:
-                global_category = mapping['global_category_id']
+            if mapping["local_category_id"] == item_local_category:
+                global_category = mapping["global_category_id"]
                 break
         if not global_category:
             return item_local_category
-        global_category_label = utils_view.get_categories_mappings_value(global_category)
+        global_category_label = utils_view.get_categories_mappings_value(
+            global_category
+        )
         return global_category_label if global_category_label else global_category
