79959f10e3e35ef41131de5e5d38b0bb93a904bb
Olivier Delaere <olivier.delaere@imio.be>
2019-11-25 14:58:07 +0100

    Added plonemeeting_last_modified

--- CHANGES.rst
+++ CHANGES.rst
@@ -5,6 +5,8 @@ Changelog
 1.0a3 (unreleased)
 ------------------
 
+- Added plonemeeting_last_modified to keep track of sync status more easily
+  [odelaere]
 - Restrict import action to institutions
   [laulaz]
 
--- src/plonemeeting/portal/core/content/item.py
+++ src/plonemeeting/portal/core/content/item.py
@@ -51,6 +51,10 @@ class IItem(model.Schema):
 
     extra_info = RichText(title=_(u"Extra info"), required=False)
 
+    plonemeeting_last_modified = schema.Datetime(
+        title=_(u"Last modification in ia.Delib"), required=True, readonly=True,
+    )
+
 
 @implementer(IItem)
 class Item(Container):
