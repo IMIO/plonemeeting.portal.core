c600f7636b009d3f1e69012db4670f1ee284686f
Olivier Delaere <olivier.delaere@imio.be>
2019-12-05 13:24:24 +0100

    Fixed robot

--- src/plonemeeting/portal/core/content/institution.py
+++ src/plonemeeting/portal/core/content/institution.py
@@ -70,7 +70,7 @@ class IInstitution(model.Schema):
     logo = NamedBlobImage(title=_(u"Logo"), required=False)
 
     item_decision_formatting_tal = schema.TextLine(
-        title=_(u"Item decision formatting tal expression"), required=True
+        title=_(u"Item decision formatting tal expression"), required=False
     )
 
     info_annex_formatting_tal = schema.TextLine(
