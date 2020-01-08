ec4503b3d7350b4549b418510f89bf6f04853bc5
Gauthier Bastien <g.bastien@imio.be>
2020-01-08 17:35:35 +0100

    Post release

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,6 +2,12 @@ Changelog
 =========
 
 
+1.0rc3 (unreleased)
+-------------------
+
+- Nothing changed yet.
+
+
 1.0rc2 (2020-01-08)
 -------------------
 
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="plonemeeting.portal.core",
-    version="1.0rc2",
+    version="1.0rc3.dev0",
     description="Plonemeeting decisions publication portal",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
