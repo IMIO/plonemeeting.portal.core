404eaa5acf9574914838c670b69f66e02aa97ff3
Laurent Lasudry <lasudry@gmail.com>
2020-01-15 11:05:52 +0100

    Add more tests on institution creation / deletion

--- src/plonemeeting/portal/core/tests/test_ct_institution.py
+++ src/plonemeeting/portal/core/tests/test_ct_institution.py
@@ -3,6 +3,7 @@ from plonemeeting.portal.core.content.institution import IInstitution
 from plonemeeting.portal.core.testing import (
     PLONEMEETING_PORTAL_CORE_INTEGRATION_TESTING,
 )  # noqa
+from plonemeeting.portal.core.utils import format_institution_managers_group_id
 from plone import api
 from plone.api.exc import InvalidParameterError
 from plone.app.testing import setRoles
@@ -49,6 +50,14 @@ class InstitutionIntegrationTest(unittest.TestCase):
             container=self.portal, type="Institution", id="institution"
         )
 
+        group_id = format_institution_managers_group_id(obj)
+        roles = api.group.get_roles(groupname=group_id)
+        self.assertEqual(roles, ["Authenticated"])
+        self.assertTupleEqual(
+            obj.get_local_roles_for_userid(group_id),
+            ("Institution Manager", "Contributor"),
+        )
+
         self.assertTrue(
             IInstitution.providedBy(obj),
             u"IInstitution not provided by {0}!".format(obj.id),
@@ -60,6 +69,7 @@ class InstitutionIntegrationTest(unittest.TestCase):
         # check that deleting the object works too
         api.content.delete(obj=obj)
         self.assertNotIn("institution", parent.objectIds())
+        self.assertIsNone(api.group.get(group_id))
 
     def test_ct_institution_globally_addable(self):
         setRoles(self.portal, TEST_USER_ID, ["Manager"])
