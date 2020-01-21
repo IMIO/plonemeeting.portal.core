62047c370f0e78bb20043fc37947e721ac82638f
Olivier Delaere <olivier.delaere@imio.be>
2020-01-21 11:41:56 +0100

    Added password policy tests

--- src/plonemeeting/portal/core/passwords.py
+++ src/plonemeeting/portal/core/passwords.py
@@ -9,9 +9,10 @@ from plonemeeting.portal.core import _
 @implementer(ICustomPasswordValidator)
 class PloneMeetingPasswordValidator(object):
 
-    def __init__(self, context):
+    def __init__(self, context):  # pragma: no cover
         self.context = context
 
-    def validate(self, password, data):
+    @staticmethod
+    def validate(password, data):
         if len(password) < 8:
             return _(u"Passwords must be at least 8 characters in length.")
---
+++ src/plonemeeting/portal/core/tests/test_password.py
@@ -0,0 +1,34 @@
+import unittest
+
+from plonemeeting.portal.core.passwords import PloneMeetingPasswordValidator
+from plonemeeting.portal.core.testing import PLONEMEETING_PORTAL_DEMO_FUNCTIONAL_TESTING
+
+
+class TestPloneMeetingPasswordValidator(unittest.TestCase):
+
+    layer = PLONEMEETING_PORTAL_DEMO_FUNCTIONAL_TESTING
+
+    def test_validate(self):
+        invalid_label = "Passwords must be at least 8 characters in length."
+        self.assertEqual(
+            invalid_label, PloneMeetingPasswordValidator.validate("", None)
+        )
+
+        self.assertEqual(
+            invalid_label, PloneMeetingPasswordValidator.validate("1234567", None)
+        )
+
+        self.assertIsNone(PloneMeetingPasswordValidator.validate("12345678", None))
+        self.assertIsNone(PloneMeetingPasswordValidator.validate("        ", None))
+        self.assertIsNone(PloneMeetingPasswordValidator.validate("azertyui", None))
+        self.assertIsNone(
+            PloneMeetingPasswordValidator.validate("azertyuiopqsdfghjklmwxcvbn", None)
+        )
+        self.assertIsNone(PloneMeetingPasswordValidator.validate("7V4#VAku", None))
+        self.assertIsNone(
+            PloneMeetingPasswordValidator.validate(
+                "cZ2mtREhxp6CTPEoyz6Aqs$5BvubreUjpo7fKHB*Ng6KVaMFB4P77Gi!5Q5oEiCSDh^"
+                "xKDSe#aRSb%iT@7upnq4C&gZoVffZj^#M9kZS$bhBsyqsvhDKnNbZy#M$p3Hb",
+                None,
+            )
+        )
