287eec9a7effb609d3a7ddd8a82d29b79ea8ebd7
Olivier Delaere <23073839+odelaere@users.noreply.github.com>
2021-09-24 16:49:24 +0200

    filter output in item decision for anonimyzation

--- src/plonemeeting/portal/core/filters/replace_masked_gdpr.py
+++ src/plonemeeting/portal/core/filters/replace_masked_gdpr.py
@@ -2,8 +2,11 @@
 from plone.api.portal import get_navigation_root
 from plone.api.portal import get_registry_record
 from plone.outputfilters.interfaces import IFilter
+from plonemeeting.portal.core.config import DELIB_ANONYMIZED_TEXT
 from plonemeeting.portal.core.config import RGPD_MASKED_TEXT
 from zope.interface import implementer
+
+import re
 
 
 @implementer(IFilter)
@@ -19,11 +22,25 @@ class ReplaceMaskedGDPR(object):
         return True
 
     def __call__(self, data):
-        import ipdb
-        ipdb.set_trace()
-        to_replace = get_registry_record("plonemeeting.portal.core.delib_masked_gdpr", RGPD_MASKED_TEXT)
+        to_replace = get_registry_record("plonemeeting.portal.core.delib_masked_gdpr", default=DELIB_ANONYMIZED_TEXT)
+        if not to_replace:  # get_registry_record may return None if record exists but empty
+            to_replace = DELIB_ANONYMIZED_TEXT
+
         if hasattr(self.institution, "url_rgpd") and self.institution.url_rgpd:
-            replace_by = self.institution.url_rgpd
+            redirect = self.institution.url_rgpd
         else:
-            replace_by = get_registry_record("plonemeeting.portal.core.rgpd_masked_text_redirect")
-        return data.replace(to_replace, replace_by)
+            default = self.institution.aq_parent.absolute_url() + "#rgpd"
+            redirect = get_registry_record("plonemeeting.portal.core.rgpd_masked_text_redirect", default=default)
+            if not redirect:  # get_registry_record may return None if record exists but empty
+                redirect = default
+
+        placeholder = get_registry_record("plonemeeting.portal.core.rgpd_masked_text_placeholder",
+                                          default=RGPD_MASKED_TEXT)
+        if not placeholder:  # get_registry_record may return None if record exists but empty
+            placeholder = RGPD_MASKED_TEXT
+
+        replace_by = '<a href="{redirect}"><span class="pm-anonymize">{placeholder}</span></a>" '.format(
+            redirect=redirect,
+            placeholder=placeholder
+        )
+        return re.sub(to_replace, replace_by, data)
