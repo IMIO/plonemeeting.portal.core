80c1f6c1c66ab96c03db2f1a2287c10d9559c991
Antoine Duchêne <antoineduchene@icloud.com>
2022-04-13 19:30:38 +0200

    Adding two new fields (meeting_type and institution_type) to Institution and start working on displaying those fields in faceted view and item view

--- src/plonemeeting/portal/core/browser/utils.py
+++ src/plonemeeting/portal/core/browser/utils.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
-
 from plone import api
 from plone.api.validation import mutually_exclusive_parameters
 from plone.app.textfield import RichTextValue
+from plone.dexterity.interfaces import IDexterityFTI
 from plone.protect.utils import addTokenToUrl
 from plonemeeting.portal.core.content.institution import IInstitution
 from plonemeeting.portal.core.content.meeting import IMeeting
 from plonemeeting.portal.core.interfaces import IMeetingsFolder
 from Products.Five.browser import BrowserView
+from Products.ZCatalog.interfaces import ICatalogBrain
+from zope.component import getUtility
 from zope.component import queryUtility
 from zope.schema.interfaces import IVocabularyFactory
 
@@ -20,6 +22,12 @@ class UtilsView(BrowserView):
     """
 
     def get_current_institution(self):
+        if ICatalogBrain.providedBy(self.context):
+            # context could be a brain in a faceted view, so we need to get the real object.
+            # Otherwise, api.portal.get_navigation_root returns the PloneSite.
+            # Which is not what we want
+            obj = self.context.getObject()
+            return api.portal.get_navigation_root(obj)
         return self.is_institution() and self.context or api.portal.get_navigation_root(self.context)
 
     def is_institution(self):
@@ -91,6 +99,24 @@ class UtilsView(BrowserView):
 
     def protect_url(self, url):
         return addTokenToUrl(url)
+
+    def vocab_title(self, context, field):
+        """ return title when given the vocabulary and the value key"""
+        schema = getUtility(IDexterityFTI, name=context.portal_type).lookupSchema()
+        if field not in schema:
+            raise ValueError(f"No such field {field} in {context.portal_type} context")
+
+        vocabulary_name = schema.get(field).vocabularyName
+        vocabulary_factory = getUtility(IVocabularyFactory, vocabulary_name)
+        vocabulary = vocabulary_factory(context)
+        return vocabulary.getTerm(getattr(context, field)).title
+
+    def meeting_type(self):
+        print(self.get_current_institution())
+        return self.vocab_title(self.get_current_institution(), "meeting_type")
+
+    def institution_type(self):
+        return self.vocab_title(self.get_current_institution(), "institution_type")
 
 
 def path_to_dx_default_template():
