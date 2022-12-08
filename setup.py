751c2b7439bcf4eeb0f8fc04c886612d1c513576
Antoine Duchêne <antoineduchene@icloud.com>
2022-12-08 15:33:28 +0100

    Back to development: 1.6.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="plonemeeting.portal.core",
-    version="1.6.0",
+    version="1.6.1.dev0",
     description="Plonemeeting decisions publication portal",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
