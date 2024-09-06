9787e462470c66fc4976ab0ee0463947c9118458
Gauthier Bastien <gauthier.bastien@imio.be>
2024-09-06 12:53:38 +0200

    Show documentby line only on real view (meeting, item, publication, institution)

--- src/plonemeeting/portal/core/viewlets/overrides.py
+++ src/plonemeeting/portal/core/viewlets/overrides.py
@@ -5,6 +5,9 @@ from plone.app.layout.viewlets.content import DocumentBylineViewlet
 
 class PMDocumentBylineViewlet(DocumentBylineViewlet):
 
+    def show(self):
+        return not self.anonymous and self.request['PUBLISHED'].__name__ == "view"
+
     def show_modification_date(self):
         """Show modified no matter it was published, useful for Publication."""
         return True
