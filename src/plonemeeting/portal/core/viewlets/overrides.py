76f37a24211ad2e127db3163d0ce4c8ff0120f52
Gauthier Bastien <gauthier.bastien@imio.be>
2024-09-09 16:19:43 +0200

    Show document by line to Manager

--- src/plonemeeting/portal/core/viewlets/overrides.py
+++ src/plonemeeting/portal/core/viewlets/overrides.py
@@ -3,6 +3,8 @@
 from plone.app.layout.viewlets.content import DocumentBylineViewlet
 from plonemeeting.portal.core.utils import is_decisions_manager
 from plonemeeting.portal.core.utils import is_publications_manager
+from Products.CMFCore.permissions import ManagePortal
+from Products.CMFCore.utils import _checkPermission
 
 
 class PMDocumentBylineViewlet(DocumentBylineViewlet):
@@ -11,7 +13,8 @@ class PMDocumentBylineViewlet(DocumentBylineViewlet):
         if not self.anonymous and self.request['PUBLISHED'].__name__ == "view":
             utils_view = self.context.restrictedTraverse("@@utils_view")
             institution = utils_view.get_current_institution()
-            return (is_decisions_manager(institution) or
+            return (_checkPermission(ManagePortal, institution) or
+                    is_decisions_manager(institution) or
                     is_publications_manager(institution))
 
     def show_modification_date(self):
