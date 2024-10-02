90f2ca4445572487b363cae666d97cb077758393
Antoine Duchêne <antoine.duchene@imio.be>
2024-10-02 16:20:47 +0200

    Back to development: 2.0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = "\n\n".join(
 )
 setup(
     name="plonemeeting.portal.core",
-    version="2.0.1",
+    version="2.0.2.dev0",
     description="Plonemeeting decisions publication portal",
     long_description=long_description,
     long_description_content_type="text/x-rst",
