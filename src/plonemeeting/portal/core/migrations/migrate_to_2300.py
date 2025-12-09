69fc73f19802daa8d9eb0eafca06ac66fd274a9c
Antoine Duchêne <antoine.duchene@imio.be>
2025-12-09 16:44:30 +0100

    Fix mixed up upgrade steps

--- src/plonemeeting/portal/core/migrations/migrate_to_2300.py
+++ src/plonemeeting/portal/core/migrations/migrate_to_2300.py
@@ -1,29 +1,19 @@
 # -*- coding: utf-8 -*-
-from imio.migrator.migrator import Migrator
+import logging
+
+from Products.CMFPlone.controlpanel.browser.quickinstaller import InstallerView
 from plone import api
-from plone.base.interfaces import IBundleRegistry
 from plone.base.utils import get_installer
-from plone.registry.interfaces import IRegistry
-from plonemeeting.portal.core.config import DEFAULT_DOCUMENTGENERATOR_TEMPLATES
 from plonemeeting.portal.core.config import PUB_FOLDER_ID
-from plonemeeting.portal.core.interfaces import IPlonemeetingPortalConfigFolder
-from plonemeeting.portal.core.setuphandlers import create_or_update_default_template
-from plonemeeting.portal.core.utils import create_templates_folder
-from plonemeeting.portal.core.utils import get_managers_group_id
+from plonemeeting.portal.core.migrations import PlonemeetingMigrator
 from plonemeeting.portal.core.utils import get_publication_creators_group_id
 from plonemeeting.portal.core.utils import get_publication_reviewers_group_id
 from plonemeeting.portal.core.utils import get_publications_managers_group_id
-from Products.CMFPlone.controlpanel.browser.quickinstaller import InstallerView
-from zope.component import getUtility
-from zope.interface import alsoProvides
-
-import logging
-
 
 logger = logging.getLogger("plonemeeting.portal.core")
 
 
-class MigrateTo2300(Migrator):
+class MigrateTo2300(PlonemeetingMigrator):
     def __init__(self, context, disable_linkintegrity_checks=False):
         super().__init__(context, disable_linkintegrity_checks)
         self.qi: InstallerView = get_installer(self.portal)
@@ -56,6 +46,7 @@ class MigrateTo2300(Migrator):
         self.ps.runImportStepFromProfile("profile-plonemeeting.portal.core:default", "plone.app.registry")
         self.ps.runImportStepFromProfile("profile-plonemeeting.portal.core:default", "typeinfo")
         self.ps.runImportStepFromProfile("profile-plonemeeting.portal.core:default", "workflow")
+        self._re_apply_faceted_configs()
         self._configure_new_publication_groups()
         logger.info("Migration to plonemeeting.portal.core 2300 done.")
 
@@ -63,7 +54,8 @@ class MigrateTo2300(Migrator):
 def migrate(context):
     """
     This migration function will:
-       1)Configure new publication groups about the new publication workflow state 'proposed'.
+       1) Re-apply faceted configuration.
+       2) Configure new publication groups about the new publication workflow state 'proposed'.
     """
     migrator = MigrateTo2300(context)
     migrator.run()
